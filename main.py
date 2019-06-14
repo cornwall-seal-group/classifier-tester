335d205f5fe6dca84e0d9cad3fdd5e03921b2a57
Rich Frost <richard@ipponsolutions.co.uk>
2019-06-14 13:44:23 +0100

    Only look for jpg images to test against

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 import config
 from api.classifier import find_seal
 
+ALLOWED_EXTENSIONS = set(['jpg'])
 ITERATION = config.ITERATION
 TEST_IMAGE_FOLDER = '../classifier-test-images/' + ITERATION + '/'
 
@@ -21,25 +22,30 @@ TEST_IMAGE_FOLDER = '../classifier-test-images/' + ITERATION + '/'
 # Submit each image to the classifier and store the result in a JSON file with the same image name
 
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 def test_images_against_classifier():
     print 'test_images_against_classifier'
     for subdir, dirs, files in os.walk(TEST_IMAGE_FOLDER):
 
         for file in files:
-            image_name = file.replace('.jpg', '')
-            image_path = os.path.join(subdir, file)
+            if allowed_file(file):
+                image_name = file.replace('.jpg', '')
+                image_path = os.path.join(subdir, file)
 
-            results = find_seal(image_path)
-            json_predictions = {}
+                results = find_seal(image_path)
+                json_predictions = {}
 
-            for prediction in results.predictions:
-                json_predictions[prediction.tag_name] = prediction.probability
-                print ("\t" + prediction.tag_name +
-                       ": {0:.2f}%".format(prediction.probability * 100))
+                for prediction in results.predictions:
+                    json_predictions[prediction.tag_name] = prediction.probability
+                    print ("\t" + prediction.tag_name +
+                           ": {0:.2f}%".format(prediction.probability * 100))
 
-            print json_predictions
-            save_json_file(subdir, image_name, json_predictions)
-            exit(1)
+                print json_predictions
+                save_json_file(subdir, image_name, json_predictions)
 
 
 def save_json_file(path, image, predictions):
